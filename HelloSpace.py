2a407de95e2b794f311cbf9a523d5d327d48e3dc
CTXz <ctx.xda@gmail.com>
2017-02-10 09:50:53 +0100

    Add HelloSpace script
    A script that allow you to greet other planets than earth

---
+++ HelloSpace.py
@@ -0,0 +1,11 @@
+# Taking a look at variables
+
+planet = "Mars"         # Define variable planet to mars
+
+print("Hello", planet); # Call planet variable(mars) and append it to "Hello"
+print("Bye", planet);   # Call planet variable(mars) and append it to "Bye"
+
+planet = "Saturn";      # Redefine planet variable to Saturn
+
+print("Hello", planet); # Redo line 5 but with planet set as Saturn
+print("Bye", planet);   # Redo line 6 but with planet set as Saturn
